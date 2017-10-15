5e749abcf9eb64d5b694d73e051c80cf3b08344c
Daria Glagol <daria.glagoll@gmail.com>
2017-10-15 20:32:56 +0700

    Prepating for checking

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 var open_btn = document.querySelector(".main-nav__toggle--close");
 var menu = document.querySelector(".nav-list");
 var close = document.querySelector(".main-nav__toggle--open");
+var menuBackground = document.querySelector(".main-nav__inner");
 
 open_btn.addEventListener("click", function(event) {
   event.preventDefault();
-  if (menu.classList.contain("nav-list--close") {
+  if (menu.classList.contains("nav-list--close")) {
     menu.classList.remove("nav-list--close");
+    open_btn.classList.add("main-nav__toggle--open");
+    menuBackground.classList.add("main-nav__inner--colored");
   } else {
     menu.classList.add("nav-list--close");
+    open_btn.classList.remove("main-nav__toggle--open");
     open_btn.classList.add("main-nav__toggle--close");
+    menuBackground.classList.remove("main-nav__inner--colored");
   }
 });
 
