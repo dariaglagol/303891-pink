84858bda2a8db1e46e3f09541a69f305c73fe626
Daria Glagol <daria.glagoll@gmail.com>
2017-10-15 22:47:21 +0700

    Add automation

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,48 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gupl-imagemin");
+var webp = require("gulp-webp");
+var run = require("run-sequence");
+var del = require("del");
+var svgstore = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+
+gulp.task("webp", function () {
+  return gupl.src("img/**/*.{png,jpg}")
+  .pipe(webp({quality: 90}))
+  .pipe(gulp.dest("img"));
+});
+
+gulp.task("sprite", function () {
+  return gulp.src("img/icon-*.svg")
+  .pipe(svgstore({
+    inlineSvg: true;
+  }))
+  .pipe(rename("sprite.svg"))
+  .pipe(gulp.dest("img"));
+});
+
+gulp.task("images", function () {
+  return gulp.src("img/**/*.{png, jpg,svg}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.svgo()
+  ]))
+  .pipe(gupl.dest("img"));
+});
+
+gulp.task("html", function () {
+  return gulp.src("*.html")
+  .pipe(posthtml([
+    include()
+  ]))
+  .pipe(gulp.dest("."));
+});
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
@@ -14,13 +56,31 @@ gulp.task("style", function() {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "fonts/**/*.{woff, woff2}",
+    "img/**",
+    "js/**"
+  ], {
+    base: "."
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
 });
 
 gulp.task("serve", ["style"], function() {
   server.init({
-    server: ".",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -28,5 +88,14 @@ gulp.task("serve", ["style"], function() {
   });
 
   gulp.watch("sass/**/*.{scss,sass}", ["style"]);
-  gulp.watch("*.html").on("change", server.reload);
+  gulp.watch("*.html", ["html"]).on("change", server.reload);
+});
+
+gulp.task("build", finction (done) {
+  run(
+    "clean",
+    "copy",
+    "style",
+    done
+  )
 });
