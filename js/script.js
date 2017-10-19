f9d937487a063af38fbc7f550423ec43d42fdf59
Daria Glagol <daria.glagoll@gmail.com>
2017-10-19 21:11:22 +0700

    Some fixes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var open_btn = document.querySelector(".main-nav__toggle--open");
+var open_btn = document.querySelector(".main-nav__toggle");
 var menu = document.querySelector(".nav-list");
 var close = document.querySelector(".main-nav__toggle--open");
 var menuBackground = document.querySelector(".main-nav__inner");
@@ -8,6 +8,7 @@ open_btn.addEventListener("click", function(event) {
   if (!menu.classList.contains("nav-list--close")) {
     menu.classList.add("nav-list--close");
     open_btn.classList.remove("main-nav__toggle--open");
+    open_btn.classList.add("main-nav__toggle--close");
     menuBackground.classList.remove("main-nav__inner--colored");
   } else {
     menu.classList.remove("nav-list--close");
